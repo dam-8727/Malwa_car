5a9b145acf9b12680b7bb40fb9cfb76773e939e4
dam-8727 <daminisingla235@gmail.com>
2024-05-08 17:27:25 +0530

    create a test api route

--- api/index.js
+++ api/index.js
@@ -1,18 +1,27 @@
   import express from'express';
   import mongoose from 'mongoose';
   import userRouter from './routes/user.route.js';
+  import authRouter from './routes/auth.route.js';
   import dotenv from 'dotenv';
   dotenv.config();
+
   mongoose
   .connect(process.env.MONGO)
   .then(() => {
     console.log('Connected to MongoDB!');
+    
   })
   .catch((err) => {
+    
     console.log(err);
+    
   });
+
+
 const app=express();
+app.use(express.json());
    app.listen(3000,()=>{
     console.log('server is running on port 3000!!!')
   });
   app.use('/api/user', userRouter);
+  app.use('/api/auth', authRouter);
